18396f791353d34124ac2e921ca138f8d7548e93
Jan Losinski <losinski@wh2.tu-dresden.de>
2019-05-16 01:23:53 +0200

    change notebook position
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -716,7 +716,7 @@ c.Spawner.default_url = '/lab'
 #  
 #  Note that this does *not* prevent users from accessing files outside of this
 #  path! They can do so with many other means.
-#c.Spawner.notebook_dir = '~/{username}/notebooks'
+c.Spawner.notebook_dir = '/notebooks/{username}'
 
 ## An HTML form for options a user can specify on launching their server.
 #  
